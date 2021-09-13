c98a3738764cb77f139b0fc0727afe19299d3242
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-09-13 02:10:15 -0300

    Estilizacao Total

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -6,7 +6,7 @@ class CardNota extends Component {
   render() {
     return (
       <section className="card-nota">
-        <header>
+        <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">Título</h3>
         </header>
         <p className="card-nota_texto">Escreva sua Nota</p>
