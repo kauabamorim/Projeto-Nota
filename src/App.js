3079f5ef440ded416f7e4ccd62421625b388dc90
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-09-13 02:22:18 -0300

    Organizando Projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ListaDeNotas from './components/ListaDeNotas/ListaDeNotas';
-import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import ListaDeNotas from './components/ListaDeNotas';
+import FormularioCadastro from './components/FormularioCadastro';
 import { Component } from 'react';
-import "./App.css"
+import "./assets/App.css"
+import "./assets/index.css"
 
 class App extends Component {
   render()  {
