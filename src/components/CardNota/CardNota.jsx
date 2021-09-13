f36b492310937d8ad34180102d2685e0283cffea
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-09-13 01:25:03 -0300

    Estilizando componentes

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Component } from "react";
+import "./estilo.css"
 
 class CardNota extends Component {
   render() {
     return (
       <section>
-        <header>
+        <header className="card-nota">
           <h3>Título</h3>
         </header>
         <p>Escreva sua Nota</p>
