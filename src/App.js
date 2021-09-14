acb18adc802e04c3e889fb269571b0e73fd2dac6
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-09-14 01:51:32 -0300

    Passando Propriedades

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import ListaDeNotas from './components/ListaDeNotas';
-import FormularioCadastro from './components/FormularioCadastro';
-import { Component } from 'react';
-import "./assets/App.css"
-import "./assets/index.css"
+import React from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import { Component } from "react";
+import "./assets/App.css";
+import "./assets/index.css";
 
 class App extends Component {
-  render()  {
-    return(
+  criarNota(titulo, texto) {
+    console.log("Uma nova nota foi criada:" + titulo + " " + texto);
+  }
+  render() {
+    return (
       <section className="conteudo">
-        <FormularioCadastro/>
-        <ListaDeNotas/>
+        <FormularioCadastro criarNota={this.criarNota} />
+        <ListaDeNotas />
       </section>
-    )
+    );
   }
 }
 
