355bd773de28351d4d33ebeea2d1877d316fd141
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-09-14 01:31:29 -0300

    Binds

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
+  constructor() {
+    super();
+    this.titulo = "";
+  }
+  handleMudancaTitulo(evento) {
+    this.titulo = evento.target.value;
+  }
+
   render() {
     return (
       <form className="form-cadastro ">
@@ -8,6 +16,7 @@ class FormularioCadastro extends Component {
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
+          onChange={this.handleMudancaTitulo.bind(this)}
         />
         <textarea
           rows={15}
