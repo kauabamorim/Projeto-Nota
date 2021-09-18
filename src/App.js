45bc0a4ef784e438ad6f04c72799417208f78848
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-09-18 16:44:58 -0300

    Finalizado o projeto

--- src/App.js
+++ src/App.js
@@ -6,14 +6,27 @@ import "./assets/App.css";
 import "./assets/index.css";
 
 class App extends Component {
-  criarNota(titulo, texto) {
-    console.log("Uma nova nota foi criada:" + titulo + " " + texto);
+  constructor() {
+    super();
+
+    this.state = {
+      notas: [],
+    };
   }
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota];
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+  
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota} />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
       </section>
     );
   }
