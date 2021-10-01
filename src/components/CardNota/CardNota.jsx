b6c91fa1aee641d2a1f102ea11c99c375a713189
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-10-01 14:46:07 -0300

    Usando SVGs

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Component } from "react";
 import "./estilo.css";
+import deleteSVG from "../../assets/img/delete.svg";
 
 class CardNota extends Component {
   render() {
@@ -8,6 +9,7 @@ class CardNota extends Component {
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <img src={deleteSVG}></img>
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
