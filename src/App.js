e09572f79a509cb2b45e4f1617d2d1771f38004e
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-10-04 15:40:14 -0300

    Deletando Cards

--- src/App.js
+++ src/App.js
@@ -17,16 +17,25 @@ class App extends Component {
     const novaNota = { titulo, texto };
     const novoArrayNotas = [...this.state.notas, novaNota];
     const novoEstado = {
-      notas:novoArrayNotas
-    }
-    this.setState(novoEstado)
+      notas: novoArrayNotas,
+    };
+    this.setState(novoEstado);
   }
-  
+
+  deletarNota(index) {
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index, 1);
+    this.setState({ nota: arrayNotas });
+  }
+
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)} />
-        <ListaDeNotas notas={this.state.notas} />
+        <ListaDeNotas
+          apagarNota={this.deletarNota.bind(this)}
+          notas={this.state.notas}
+        />
       </section>
     );
   }
--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -4,12 +4,17 @@ import "./estilo.css";
 import deleteSVG from "../../assets/img/delete.svg";
 
 class CardNota extends Component {
+  apagar() {
+    const indice = this.props.indice;
+    this.props.apagarNota(indice);
+  }
+
   render() {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
-          <img src={deleteSVG}></img>
+          <img src={deleteSVG} onClick={this.apagar.bind(this)}></img>
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
